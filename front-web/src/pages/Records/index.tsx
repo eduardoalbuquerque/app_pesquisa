6232dc636f3eba9619a042757d41a3b19e9c3b98
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-09-20 15:11:12 -0300

    front-web to production

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './pagination';
 import Filter from '../../components/Filters';
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://webdevbrasil.herokuapp.com';
 
 const Records = () => {
 
